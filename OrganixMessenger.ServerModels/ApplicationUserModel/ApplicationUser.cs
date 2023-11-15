fa0257a9279bc71dcdf389a36db81002256494d2
TeaFaris <teafaris@organizatsiya.site>
2023-11-16 02:31:11 +0500

    User and refresh token models were added

---
+++ OrganixMessenger.ServerModels/ApplicationUserModel/ApplicationUser.cs
@@ -0,0 +1,33 @@
+﻿using OrganixMessenger.Shared;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrganixMessenger.ServerModels.ApplicationUserModel
+{
+    public sealed class ApplicationUser
+    {
+        [Key]
+        public Guid Id { get; init; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        [Required]
+        public string VereficationToken { get; init; }
+
+        public string? PasswordResetToken { get; set; }
+
+        public DateTime? PasswordResetTokenExpires { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public Role Role { get; set; }
+    }
+}
