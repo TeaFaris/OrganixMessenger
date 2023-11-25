cd1ded2384cdcf11ee71bb39217c4fe3fbedb6f4
TeaFaris <teafaris@organizatsiya.site>
2023-11-26 02:31:03 +0500

    FileDTO added

---
+++ OrganixMessenger.ServerModels/FileModel/FileMapper.cs
@@ -0,0 +1,15 @@
+﻿namespace OrganixMessenger.ServerModels.FileModel
+{
+    public static class FileMapper
+    {
+        public static FileDTO ToDTO(this UploadedFile serverModel)
+        {
+            return new()
+            {
+                Id = serverModel.Id,
+                FileName = serverModel.FileName,
+                ContentType = serverModel.ContentType
+            };
+        }
+    }
+}
---
+++ OrganixMessenger.Shared/API/DTOs/File/FileDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace OrganixMessenger.Shared.API.DTOs.File
+{
+    public sealed class FileDTO
+    {
+        [Required]
+        public Guid Id { get; init; }
+
+        [Required]
+        public string FileName { get; init; }
+
+        [Required]
+        public string ContentType { get; init; }
+    }
+}
