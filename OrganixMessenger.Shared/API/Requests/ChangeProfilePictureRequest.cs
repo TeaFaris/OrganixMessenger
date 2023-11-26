5c5792b4a00b6c12d61b3cf784bfc629b71128b2
TeaFaris <teafaris@organizatsiya.site>
2023-11-26 21:15:06 +0500

    Account change password

---
+++ OrganixMessenger.Shared/API/Requests/ChangeProfilePictureRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace OrganixMessenger.Shared.API.Requests
+{
+    public sealed class ChangeProfilePictureRequest
+    {
+        [Required]
+        public IFormFile File { get; init; }
+
+        [Match("image/png", "image/jpeg")]
+        public string ContentType => File.ContentType;
+    }
+}
